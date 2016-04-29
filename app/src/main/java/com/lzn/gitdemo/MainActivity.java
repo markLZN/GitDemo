d2f548d28211a34935e969dca4a0215fda001998
markLZN <442894125@qq.com>
2016-04-29 14:55:48 +0800

    第er次提交

--- app/src/main/java/com/lzn/gitdemo/MainActivity.java
+++ app/src/main/java/com/lzn/gitdemo/MainActivity.java
@@ -2,12 +2,16 @@ package com.lzn.gitdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView aaa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        aaa = (TextView) findViewById(R.id.tv1);
     }
 }
