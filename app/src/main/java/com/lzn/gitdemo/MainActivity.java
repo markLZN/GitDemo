0de30571dbc8a920f78853d686b1f502fb4bd06a
markLZN <442894125@qq.com>
2016-04-29 15:36:37 +0800

    第二次提交

--- app/src/main/java/com/lzn/gitdemo/MainActivity.java
+++ app/src/main/java/com/lzn/gitdemo/MainActivity.java
@@ -13,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         aaa = (TextView) findViewById(R.id.tv1);
+        aaa.setText("伤害");
+
     }
 }
